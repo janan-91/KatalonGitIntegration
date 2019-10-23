59010f5ea26e7148f57ce69a06e5be9cf5e2e74d
tringapps <tringapps@10.1.0.104>
2019-10-23 12:09:09 +0530

    First Commit in github

---
+++ Test Suites/Test Suite1.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ac498850-d033-4501-938d-499912608b86</testSuiteGuid>
+   <testCaseLink>
+      <guid>4d0f7216-21b5-4d52-8379-166e94b4fbf2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sample API1/ListUsers</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9df7c37a-0b0b-4eba-806d-b83c03318370</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Sample API1/CreateUser</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
